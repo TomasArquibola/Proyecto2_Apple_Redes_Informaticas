ec34f18080362ae6cac5cbf5e2e5a95e34998425
TomasArquibola <tomasvarquibola@gamil.com>
2023-09-18 18:20:56 +0000

    formulario_nuevo

--- inicio.php
+++ inicio.php
@@ -4,14 +4,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["nombre"];
     $contraseña = $_POST["contraseña"];
 
-    // Verifica las condiciones para redirigir al usuario
     if ($apellido === "Arquibola" && $nombre === "Tomas" && $contraseña === "tom123") {
-        // Redirige al usuario a "inicio.html"
         header("Location: inicio.html");
         exit;
     } else {
-        // En caso contrario, puedes mostrar un mensaje de error o redirigir a otra página
-        echo "Los datos ingresados son incorrectos. Por favor, inténtelo de nuevo.";
+        echo "Los datos ingresados son incorrectos. Por favor, intentelo de nuevo.";
     }
 }
 ?>
