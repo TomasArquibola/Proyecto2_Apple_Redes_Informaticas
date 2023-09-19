1d7b302301d5fad4096ae6aab461928dc2fb1d76
TomasArquibola <tomasvarquibola@gmail.com>
2023-09-19 20:09:07 +0000

    nuevo html: linea de tiempo

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $apellido = $_POST["apellido"];
     $nombre = $_POST["nombre"];
-    $contraseña = $_POST["contraseña"];
+    $contraseña = $_POST["contrasenia"];
 
-    if ($apellido === "Arquibola" && $nombre === "Tomas" && $contraseña === "tom123") {
+    if ($apellido === "arquibola" && $nombre === "tomas" && $contraseña === "1234") {
         header("Location: inicio.html");
         exit;
     } else {
